377d93b8abf2bf0c1be548272b740ab9b26ca193
youchan <youchan01@gmail.com>
2018-10-15 20:39:38 +0900

    bundle gem opal-test-unit

---
+++ lib/opal/test/unit.rb
@@ -0,0 +1,9 @@
+require "opal/test/unit/version"
+
+module Opal
+  module Test
+    module Unit
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/opal/test/unit/version.rb
@@ -0,0 +1,7 @@
+module Opal
+  module Test
+    module Unit
+      VERSION = "0.1.0"
+    end
+  end
+end
